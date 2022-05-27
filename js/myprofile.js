9c7fc2fe195c8e9953296dba14226391cdf62d70
adamhaegglund <94690833+adamhaegglund@users.noreply.github.com>
2022-05-27 18:38:43 +0200

    fixat med user

---
+++ js/myprofile.js
@@ -0,0 +1,44 @@
+function updateUsername(){
+    var x = document.getElementById("updateUsername");
+    if(x.style.display == "none"){
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function updateFirstname(){
+    var x = document.getElementById("updateFirstname");
+    if(x.style.display == "none"){
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function updateLastname(){
+    var x = document.getElementById("updateLastname");
+    if(x.style.display == "none"){
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function updateEmail(){
+    var x = document.getElementById("updateEmail");
+    if(x.style.display == "none"){
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function updatePassword(){
+    var x = document.getElementById("updatePassword");
+    if(x.style.display == "none"){
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+}
